2783702159af99982e035cb98b1bf09b8e3bca39
Gabe Jensen <gabe.jensen@economicmodeling.com>
2020-02-18 12:43:38 -0800

    small change to test out zeit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Gabe's Dev Site
+          Gabe's Dev Site 2.0
         </p>
       </header>
     </div>
