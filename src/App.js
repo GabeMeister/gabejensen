8a2335d50be455b7e80fda4b51d56f019553266e
Gabe Jensen <gabe.jensen@economicmodeling.com>
2020-03-08 16:40:28 -0700

    Started using react router and material ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link as RouterLink
+} from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import Link from '@material-ui/core/Link';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Gabe's Dev Site 2.0
-        </p>
-      </header>
-    </div>
-  );
+import AboutPage from './components/AboutPage';
+import HomePage from './components/HomePage';
+import LinksPage from './components/LinksPage';
+
+const useStyles = makeStyles({
+	toolbar: {
+		justifyContent: 'space-between'
+	},
+	navbarBtn: {
+		color: 'white'
+	},
+	navbarLink: {
+		color: 'white'
+	},
+	btnGroup: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		width: '200px'
+	},
+	appBody: {
+		boxSizing: 'border-box',
+		padding: '10px 30px'
+	}
+});
+
+const App = () => {
+	const classes = useStyles();
+
+	return (
+		<Router>
+			<div className="App">
+				<header>
+					<AppBar position="static">
+						<Toolbar className={classes.toolbar}>
+							<IconButton edge="start">
+								<MenuIcon />
+							</IconButton>
+							<Typography variant="h6">Gabe Jensen's Portfolio</Typography>
+							<nav className={classes.btnGroup}>
+								<Typography variant="h6"><Link className={classes.navbarLink} component={RouterLink} to="/">Home</Link></Typography>
+								<Typography variant="h6"><Link className={classes.navbarLink} component={RouterLink} to="/about">About</Link></Typography>
+								<Typography variant="h6"><Link className={classes.navbarLink} component={RouterLink} to="/links">Links</Link></Typography>
+							</nav>
+						</Toolbar>
+					</AppBar>
+				</header>
+
+				<div className={classes.appBody}>
+					<Switch>
+						<Route path="/about">
+							<AboutPage />
+						</Route>
+						<Route path="/links">
+							<LinksPage />
+						</Route>
+						<Route path="/">
+							<HomePage />
+						</Route>
+					</Switch>
+				</div>
+			</div>
+		</Router >
+	);
 }
 
 export default App;
---
+++ src/components/LinksPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LinkePage = () => {
+    return (
+        <h1>Links Page</h1>
+    );
+};
+
+export default LinkePage;
