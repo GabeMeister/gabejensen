30fbfb9f671b0ab392b84cecedf0a5f394f6ac4f
Gabe Jensen <gabe.jensen@economicmodeling.com>
2020-03-22 20:11:19 -0700

    Got quotes lookin better

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,37 +1,64 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
+import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
+
+import Spacer from '../Spacer';
 
 const useStyles = makeStyles({
     landingPage: {
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center'
+        alignItems: 'center',
+        width: '800px',
+        margin: 'auto',
+        paddingBottom: '200px',
+    },
+    quoteSection: {
+        width: '400px',
+        lineHeight: '32px',
+    },
+    quoteAuthorText: {
+        display: 'flex',
+        justifyContent: 'flex-end'
     }
 })
 
 const HomePage = () => {
     const classes = useStyles();
-    const [url, setUrl] = useState('');
+    const [quote, setQuote] = useState('');
 
     useEffect(() => {
         fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
             .then(resp => resp.json())
             .then(resp => {
-                setUrl(resp[0]);
+                setQuote(resp[0]);
             })
             .catch(resp => {
-                setUrl('Error fetching quote!');
+                setQuote('Error fetching quote!');
             });
     }, []);
 
     return (
         <div className={classes.landingPage}>
-            <h1>What's up. I'm Gabe.</h1>
-            {url
+            <Spacer verticalSize="60px" />
+            <Typography variant="h1">What's up.</Typography>
+            <Spacer verticalSize="80px" />
+            <Typography variant="h2">I'm Gabe.</Typography>
+            <Spacer verticalSize="20px" />
+            <Typography variant="h2">And I like web development.</Typography>
+            <Spacer verticalSize="200px" />
+            {quote
                 ? (
-                    <span>{url}</span>
+                    <div className={classes.quoteSection}>
+                        <FormatQuoteIcon />
+                        <Typography variant="body1" display="inline" color="textSecondary">{quote}</Typography>
+                        <FormatQuoteIcon />
+                        <div className={classes.quoteAuthorText}>
+                            <Typography variant="caption">- Ron Swanson</Typography>
+                        </div>
+                    </div>
                 ) : (
                     <CircularProgress />
                 )
